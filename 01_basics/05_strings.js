ea2d89529fb0e9fd0313fe43ac83c8d990f95c80
USER <81822234+USER69x69@users.noreply.github.com>
2024-11-15 07:27:06 +0000

    add strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "Sourabh"
+const repoCount = 1
+
+// console.log(name + repoCount + " Value");  //outdated 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Sourabh');
+
+console.log(gameName.toUpperCase());
+
+const newStringOne = "           Sourabh             ";
+console.log((newStringOne.trim()).length);
+
+const url = "https://example.com/sourabh%20prasad";
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('fme'));
